c850d4f494b05ae285c88845355a826497b027d8
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-05-25 11:10:05 +0100

    Add alt prop for the image tag

--- src/CountriesDetail.js
+++ src/CountriesDetail.js
@@ -5,7 +5,7 @@ const CountriesDetail = (props) => {
     ({ name, population, region, capital, flag }, index) => {
       return (
         <div className="country-detail-container" key={index}>
-          <img src="https://images.unsplash.com/photo-1604948067797-fe4288ffef04?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1215" />
+          <img src="https://images.unsplash.com/photo-1604948067797-fe4288ffef04?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1215" alt="countries flags"/>
           <div className="country">
             <h3>{name}</h3>
             <p>
